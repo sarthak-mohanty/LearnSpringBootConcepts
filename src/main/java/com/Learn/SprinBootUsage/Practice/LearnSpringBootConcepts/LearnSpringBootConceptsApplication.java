18277e4c1f3144793e2b955d5e75d582560577b6
sarthak-mohanty <sarthak0198@gmail.com>
2023-11-06 20:34:21 +0530

    Initial commit

---
+++ src/main/java/com/Learn/SprinBootUsage/Practice/LearnSpringBootConcepts/LearnSpringBootConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.Learn.SprinBootUsage.Practice.LearnSpringBootConcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootConceptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Learn/SprinBootUsage/Practice/LearnSpringBootConcepts/LearnSpringBootConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Learn.SprinBootUsage.Practice.LearnSpringBootConcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
